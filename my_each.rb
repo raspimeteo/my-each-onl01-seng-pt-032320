db48a8a3923898ebf4c3e82689e9602f25b1d3d0
John Zonneveld <jjczon@gmail.com>
2020-03-28 17:49:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -7,8 +7,6 @@ def my_each(collection)# put argument(s) here
   while i < collection.length
     yield(collection[i])
     puts "array is #{yield}"
-  else
-    puts "Hey no block was given"
     i = i + 1
   end
  
